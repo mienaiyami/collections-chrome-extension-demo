8150907e032b80f2d0c3de49854a674ba025fe79
Yami <sukoonkumar2@gmail.com>
2021-08-29 16:58:50 +0530

    open selected link

---
+++ build/index.js
@@ -0,0 +1,6 @@
+// document.querySelectorAll("a").forEach((i) => {
+//     i.onclick = (e) => {
+//         e.preventDefault();
+//         // chrome.tabs.create({ url: e.target.getAttribute("href") });
+//     };
+// });
